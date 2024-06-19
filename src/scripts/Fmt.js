f21559ddaeb21d3391c009b82fcae2874b0a8cac
hxAri <hxari@proton.me>
2024-06-19 13:45:20 +0700

    [UPDATE] Updated

--- src/scripts/Fmt.js
+++ src/scripts/Fmt.js
@@ -2,7 +2,7 @@
 // Import Scripts
 import Type from "/src/scripts/Type.js";
 
-/*
+/**
  * String formater.
  *
  * @params String format
@@ -10,8 +10,8 @@ import Type from "/src/scripts/Type.js";
  *
  * @return String
  */
-export default function Fmt( format, ...values )
-{
+export default function Fmt( format, ...values ) {
+
 	var i = 0;
 	var index = 0;
 	var match = null;
@@ -20,26 +20,24 @@ export default function Fmt( format, ...values )
 	var regexp = /(?:(?<except>\\{0,})(?<format>(\{[\s\t]*((?<key>[a-zA-Z_\x80-\xff]([a-zA-Z0-9_\x80-\xff]{0,}[a-zA-Z0-9_\x80-\xff]{1})*)|(?<index>[\d]+))*[\s\t]*\})))/igms;
 	
 	// If format value is String type.
-	if( Type( format, String ) )
-	{
-		while( ( match = regexp.exec( format ) ) !== null )
-		{
+	if( Type( format, String ) ) {
+		while( ( match = regexp.exec( format ) ) !== null ) {
+			
 			slash = "";
 			result += format.substring( index, regexp.lastIndex - match[0].length );
 			index = regexp.lastIndex;
 			
 			// If backslash character exists.
-			if( Type( match.groups.backslash, String ) )
-			{
+			if( Type( match.groups.backslash, String ) ) {
+				
 				// Get backslash length.
 				var length = length = match.groups.backslash.length;
 				
 				// If the number of backslashes is more than one.
-				if( length !== 1 )
-				{
+				if( length !== 1 ) {
+					
 					// If number of backslash is odd.
-					if( length % 2 !== 0 )
-					{
+					if( length % 2 !== 0 ) {
 						result += "\\".repeat( length -1 );
 						result += match.groups.format;
 						continue;
@@ -58,21 +56,16 @@ export default function Fmt( format, ...values )
 			result += slash;
 			
 			// If Index or Key is exists in groups.
-			if( Type( match.groups.key, String ) ||
-				Type( match.groups.index, String ) )
-			{
-				// If format by key name.
-				if( Type( match.groups.key, String ) )
-				{
+			if( Type( match.groups.key, String ) || Type( match.groups.index, String ) ) {
+				
+					// If format by key name.
+				if( Type( match.groups.key, String ) ) {
 					// If value of element 1 on array args Object type.
-					if( Type( values[0], Object ) )
-					{
+					if( Type( values[0], Object ) ) {
 						result += values[0][match.groups.key] ?? "";
 					}
 				}
 				else {
-					
-					// Get value by index.
 					result += values[match.groups.index] ?? "";
 				}
 				continue;
@@ -81,7 +74,7 @@ export default function Fmt( format, ...values )
 			// Get value by index iteration.
 			result += values[i++] ?? "";
 		}
-		return( result + format.substring( index ) );
+		return result + format.substring( index );
 	}
 	throw new TypeError( `Argument format must be type String, ${Type( format )} given` );
 };
