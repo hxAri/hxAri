58523d997cf989cd3adcd48f3dbf1129eb9be683
hxari <hxari@proton.me>
2025-03-01 20:54:57 +0700

    [UPDATE] Updated src/directives/ScrollReveal.js

--- src/directives/ScrollReveal.js
+++ src/directives/ScrollReveal.js
@@ -2,8 +2,7 @@
 import ScrollReveal from "scrollreveal";
 
 export default {
-	mounted: function( el, binding )
-	{
+	mounted: function( el, binding ) {
 		const options = binding.value || {};
 		ScrollReveal().reveal( el.childNodes, { reset: true }, options.sequenceDelay );
 	}
