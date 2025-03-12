435a8c3cb9167e8d9e20472236c2d9df226f5217
hxari <hxari@proton.me>
2025-03-13 01:59:06 +0700

    [UPDATE] Updated src/scripts/shells/commands/Echo.js

--- src/scripts/shells/commands/Echo.js
+++ src/scripts/shells/commands/Echo.js
@@ -1,6 +1,7 @@
 
 // Import Scripts
 import Author from "/src/scripts/Author.js";
+import Type from "/src/scripts/Type.js";
 
 export default {
 	name: "echo",
@@ -12,6 +13,11 @@ export default {
 	abouts: "Display text on the terminal",
 	options: {
 		e: {
+			type: Boolean,
+			usage: "Enable interpretation of backslash escapes",
+			require: false
+		},
+		E: {
 			type: Boolean,
 			usage: "Enable interpretation of backslash escapes",
 			require: false
@@ -29,48 +35,48 @@ export default {
 		 *
 		 * @return String
 		 */
-		toString: function()
-		{
-			return( this ).$argv.slice( this.$args.e === true || this.$args.n === true ? 2 : 1 ).join( "\x20" );
+		toString: function() {
+			return this.$argv.slice( this.$args.e === true || this.$args.n === true ? 2 : 1 ).join( "\x20" );
 		}
 	},
-	mounted: function({ e, n } = {})
-	{
-		if( e === true && n === true ) throw "echo: To many arguments";
-		if( e === true )
-		{
+	mounted: function({ e, n, E } = {}) {
+		
+		var result = "";
+		
+		if( e && n && E ) {
+			throw new Error( "echo: To many arguments" );
+		}
+		if( e === true ) {
 			var string = this.toString();
-			var result = "";
 			var match = null;
 			var index = 0;
-			
-			while( ( match = this.regexp.exec( string ) ) !== null )
-			{
+			while( Type( match = this.regexp.exec( string ), Object ) ) {
 				result += string.substring( index, this.regexp.lastIndex - match[0].length );
 				index = this.regexp.lastIndex;
-				
-				switch( true )
-				{
-					case match[0] === "\\r":
+				switch( true ) {
+					case match[0] === "\x5c\x72":
 						result = ""; break;
-					case match[0] === "\\n":
-						result += "\n"; break;
-					case match[0] === "\\s":
+					case match[0] === "\x5c\x6e":
+						result += "\x0a"; break;
+					case match[0] === "\x5c\x73":
 						result += "\x20"; break;
-					case match[0] === "\\t":
+					case match[0] === "\x5c\x74":
 						result += "\x20\x20\x20\x20"; break;
-					case match[0] === "\\v":
-						result += "\n\x20\x20\x20\x20"; break;
-					case match[0] === "\\b":
+					case match[0] === "\x5c\x76":
+						result += "\x0a\x20\x20\x20\x20"; break;
+					case match[0] === "\x5c\x62":
 						result = result.replace( /\s*$/, "" ); break;
 					default:
-						result += "\\"; break;
+						result += "\x5c"; break;
 				}
 			}
-			return( result + string.substring( index ) ).split( "\n" );
+			result+= string.substring( index );
 		}
 		else {
-			return([ this.toString() ]);
+			result+= this.toString();
 		}
+		return {
+			stdout: result.split( "\x0a" )
+		};
 	}
 };
