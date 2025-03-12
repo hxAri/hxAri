b489aee8a91da31832bd96c957e26919b42ad223
hxari <hxari@proton.me>
2025-03-13 01:58:35 +0700

    [UPDATE] Updated src/scripts/shells/commands/Cd.js

--- src/scripts/shells/commands/Cd.js
+++ src/scripts/shells/commands/Cd.js
@@ -11,38 +11,31 @@ export default {
 	abouts: "Change the shell working directory",
 	methods: {
 	},
-	mounted: function()
-	{
+	mounted: function() {
+
 		// Remove command name from argument values.
 		var argv = this.$argv.slice( 1 );
-		try
-		{
-			// Check for avoid to many arguments.
-			if( argv.length > 1 ) throw "To many arguments";
-			
-			// Resolve pathname.
-			var name = this.$root.pathResolver( argv[0] = argv[0] ?? this.$envs.HOME, true );
-			
-			// Try to check directory.
-			var path = this.$root.ls( name );
-			
-			// Check if path is not path.
-			if( Not( path, Array ) )
-			{
-				throw Fmt( "{}: Is not a directory", argv[0] );
-			}
-			
-			// Save old directory destination.
-			this.$envs['PWD-OLD'] = this.$root.pwd();
-			
-			// Push route path.
-			this.$root.router.push(
-				Fmt( "/terminal/{}", name ).replaceAll( /\/\//g, "/" ).replace( /\/$/g, "" )
-			);
+		
+		// Check for avoid to many arguments.
+		if( argv.length > 1 ) throw new Error( "cd: to many arguments" );
+
+		// Resolve pathname.
+		var name = this.$root.pathResolver( argv[0] = argv[0] ?? this.$envs.HOME, true );
+		
+		// Try to check directory.
+		var path = this.$root.ls( name );
+		
+		// Check if path is not path.
+		if( Not( path, Array ) ) {
+			throw new Error( Fmt( "cd: {}: is not a directory", argv[0] ) );
 		}
-		catch( error )
-		{
-			throw Fmt( "cd: {}", error );
-		}
+		
+		// Save old directory destination.
+		this.$envs['PWD-OLD'] = this.$root.pwd();
+		
+		// Push route path.
+		this.$root.router.push(
+			Fmt( "/terminal/{}", name ).replaceAll( /\/\//g, "/" ).replace( /\/$/g, "" )
+		);
 	}
 };
