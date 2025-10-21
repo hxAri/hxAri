563d7b779e5179589a3e120abd534b7d483321e3
hxari <hxari@proton.me>
2025-10-22 02:22:17 +0700

    [UPDATED] Updated src/routing/router.js

--- src/routing/router.js
+++ src/routing/router.js
@@ -1,8 +1,37 @@
 
+/**
+ * 
+ * hxAri | router.js
+ * 
+ * @author hxAri
+ * @github https://github.com/hxAri/hxAri
+ * @license MIT
+ * 
+ * Copyright (c) 2022 Ari Setiawan | hxAri
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
 import { createRouter, createWebHistory } from "vue-router";
 
-// Import Routes.
-import Routes from "/src/routing/routes.js";
+import { Routes } from "./routes";
 
 // The router instance.
 const router = createRouter({
@@ -15,13 +44,14 @@ const router = createRouter({
 	routes: Routes,
 	
 	/**
-	 * Scroll Behavior.
+	 * Scroll Behavior
 	 *
-	 * @params Mixed to
-	 * @params Mixed from
-	 * @params Mixed save
+	 * @param {8} to
+	 * @param {*} from
+	 * @param {*} save
 	 *
-	 * @return Mixed
+	 * @returns {Object}
+	 * 
 	 */
 	scrollBehavior: function( to, from, save ) {
 		if( to.hash ) {
@@ -46,6 +76,6 @@ const router = createRouter({
 			behavior: "smooth"
 		};
 	}
-})
+});
 
-export default router;
+export { router as Router };
