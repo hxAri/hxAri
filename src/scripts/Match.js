7b36b98feb92302239d039e3fcea53d5e26dd4eb
hxari <hxari@proton.me>
2025-10-22 02:22:19 +0700

    [UPDATED] Updated src/scripts/Match.js

--- src/scripts/Match.js
+++ src/scripts/Match.js
@@ -1,28 +1,58 @@
 
-// Importing application scripts.
-import Callable from "/src/scripts/types/Callable.js";
-import Not from "/src/scripts/logics/Not.js";
-import Type from "/src/scripts/Type.js";
+/**
+ * 
+ * hxAri | match.js
+ * 
+ * @author hxAri
+ * @github https://github.com/hxAri/hxAri
+ * @license MIT
+ * 
+ * Copyright (c) 2022 Ari Setiawan | hxAri
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+import { Not } from "/src/scripts/logics";
+import { Callable, Typed } from "/src/scripts/types";
 
 /**
  * Check if value is callable or function.
  *
- * @params Mixed $match
+ * @param {T} match
  *
- * @return Boolean
+ * @returns {Boolean}
+ * 
  */
-const isCall = match => typeof match === "function" || Type( match, String ) && /^function/.test( match );
+const isCall = match => typeof match === "function" || Typed( match, String ) && /^function/.test( match );
 
 /**
  * Check if match case is default option.
  *
- * @params Array $cases
+ * @param {Array<T>} cases
  *
- * @return Mixed
+ * @returns {T}
+ * 
  */
 const isDefault = cases => {
 	for( let i in cases ) {
-		if( Type( cases[i].none, Boolean ) && cases[i].none ) {
+		if( Typed( cases[i].none, Boolean ) && cases[i].none ) {
 			return i;
 		}
 	}
@@ -32,43 +62,46 @@ const isDefault = cases => {
 /**
  * Check if value is multiple match case.
  *
- * @params Mixed $match
+ * @param {T} match
  *
- * @return Boolean
+ * @returns {Boolean}
+ * 
  */
-const isMultiple = match => Type( match, Object ) && Type( match.case, Array ) && Type( match.call ) !== "Undefined";
+const isMultiple = match => Typed( match, Object ) && Typed( match.case, Array ) && Typed( match.call ) !== "Undefined";
 
 /**
  * Return match result.
  *
- * @params Mixed $match
- * @params Mixed $value
+ * @param {T} match
+ * @param {T} value
  *
- * @return Mixed
+ * @returns {T}
+ * 
  */
 const returns = ( match, value ) => isCall( value ) ? value( match ) : value;
 
 /**
  * Match cases function.
  *
- * @params Mixed $match
- * @params Array $cases
+ * @param {T} match
+ * @param {Array<T>} cases
  *
- * @return Mixed
+ * @returns {T}
+ * 
  */
 export default function Match( match, cases ) {
-	if( Type( cases, Array ) ) {
+	if( Typed( cases, Array ) ) {
 		var post = isDefault( cases );
 		for( let i in cases ) {
 			if( i !== post ) {
-				if( Type( cases[i].case, Array ) ) {
+				if( Typed( cases[i].case, Array ) ) {
 					for( let u in cases[i].case ) {
-						if( Type( match, cases[i].case[u] ) || match === cases[i].case[u] ) {
+						if( Typed( match, cases[i].case[u] ) || match === cases[i].case[u] ) {
 							return Callable( cases[i].call );
 						}
 					}
 				}
-				else if( Type( match, cases[i].case ) || match === cases[i].case ) {
+				else if( Typed( match, cases[i].case ) || match === cases[i].case ) {
 					return Callable( cases[i].call );
 				}
 			}
