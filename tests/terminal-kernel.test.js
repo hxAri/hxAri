a1a61a563a22027fab18bd78553ab137e55a7489
hxari <hxari@proton.me>
2026-04-22 20:44:07 +0700

    [UPDATE] Updated tests/terminal-kernel.test.js

--- tests/terminal-kernel.test.js
+++ tests/terminal-kernel.test.js
@@ -1,48 +1,92 @@
 
+/**
+ * 
+ * hxAri | alias.js
+ * 
+ * @author hxAri
+ * @github https://github.com/hxAri/hxAri
+ * @license MIT
+ * 
+ * Copyright (c) 2022 Ari Setiawan | hxAri
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
 "use strict";
 
-import Router from "../src/routing/router";
-import { Terminal } from "../src/scripts/terminal";
-import { test } from "vitest";
+import { expect, test } from "vitest";
+
+import { Router } from "/src/routing/router";
+import { Kernel } from "/src/scripts/terminal/kernel";
 
 
-const terminal = new Terminal( null, Router, null );
-const kernel = terminal.kernel;
+const kernel = new Kernel( Router );
+const group = kernel.group;
 const root = kernel.root;
-const self = terminal.hxari;
 
 
-test( "Kernel.allocateGID", () => kernel.allocateGID() );
-test( "Kernel.allocatePID", () => kernel.allocatePID() );
-test( "Kernel.allocateUID", () => kernel.allocateUID() );
-test( "Kernel.groupadd", () => {
+test.only( "Kernel.allocateGID", function() {
+	expect( kernel.allocateGID() ).toBe( 1001 ); 
+});
+
+test.only( "Kernel.allocatePID", function() {
+	expect( kernel.allocatePID() ).toBe( 101 );
+});
+
+test.only( "Kernel.allocateUID", function() {
+	expect( kernel.allocateUID() ).toBe( 1000 );
+});
+
+test.only( "Kernel.groupadd", function() {
 	kernel.groupadd( "testing", { user: root } );
 });
-test( "Kernel.groupdel", () => {
+
+test.only( "Kernel.groupdel", function() {
 	kernel.groupdel( "testing", { user: root } );
 });
-test( "Kernel.groupmod", () => {
+
+test.only( "Kernel.groupmod", function() {
+	kernel.groupmod( root.group, { user: root } );
 });
-test( "Kernel.kill", () => {
+
+test.only( "Kernel.groupres", function() {
+	expect( kernel.groupres( root.group ) ).toBe( group.gid );
 });
-test( "Kernel.spawn", () => {
+
+test.only( "Kernel.switch", function() {
+	kernel.switch( "root" );
 });
-test( "Kernel.switch", () => { // <<< su
-	kernel.switch( self.username );
+
+test.only( "Kernel.user", function() {
+	expect( kernel.user() ).toBe( root );
 });
-test( "Kernel.useradd", () => {
-	kernel.useradd( "virtual", {
-		password: "KIMCIL JAHAT!",
-		user: root
-	});
+
+test.only( "Kernel.useradd", function() {
+	kernel.useradd( "testing", { user: root } );
 });
-test( "Kernel.userdel", () => {
-	kernel.userdel( "virtual", { user: root } );
+
+test.only( "Kernel.userdel", function() {
+	kernel.userdel( "testing", { user: root } );
 });
-test( "Kernel.usermod", () => {
-	kernel.usermod( self.username, {
-		group: "sudo",
-		user: root
-	});
-	console.debug( "/:", JSON.stringify( kernel.vfs.root.object(), null, 4 ) );
+
+test.only( "Kernel.usermod", function() {
+	kernel.usermod( root.username, { home: "/home/root", user: root } );
 });
+
